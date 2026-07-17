2fc997c2c2ece2aba41fee11b80856b4b3963b7f
LateLammas <latelamas@gmail.com>
2026-07-17 22:09:13 +0300

    Update visaGame.js

--- game/visaGame.js
+++ game/visaGame.js
@@ -1,1 +1,9 @@
-d
+// game/visaGame.js
+import { switchRoster } from '../data/roster.js';
+import { startGame } from './render.js';
+
+// Read the roster the user chose on the main page, and load it into the database
+const savedName = typeof sessionStorage !== 'undefined' ? sessionStorage.getItem('activeRosterName') : null;
+switchRoster(savedName || 'In MM12 We Trust');
+
+startGame();
