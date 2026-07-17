a3821f813b66a4b7a1cdd711851dbe0a670b477c
LateLammas <latelamas@gmail.com>
2026-07-17 22:46:03 +0300

    Update visaGame.js

--- game/visaGame.js
+++ game/visaGame.js
@@ -1,9 +1,11 @@
 // game/visaGame.js
-import { switchRoster } from '../data/roster.js';
+import { switchRoster, customRosters, activeRosterName } from '../data/roster.js';
 import { startGame } from './render.js';
 
 // Read the roster the user chose on the main page, and load it into the database
 const savedName = typeof sessionStorage !== 'undefined' ? sessionStorage.getItem('activeRosterName') : null;
-switchRoster(savedName || 'In MM12 We Trust');
+// Validate saved name, otherwise fall back to the default roster (Olympiahybridi)
+const rosterName = (savedName && customRosters[savedName]) ? savedName : activeRosterName;
+switchRoster(rosterName);
 
 startGame();
